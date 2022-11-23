d320d675e3cec53bbd7707231a9056e9c46ab653
Simon <simon@7lieues.io>
2022-11-23 17:13:41 +0100

    style: puts non breakable spaces for better diplay on mobiles

--- components/header.js
+++ components/header.js
@@ -3,7 +3,7 @@ export default function header() {
     <header>
       <h1>Chômedu Weather</h1>
       <h2 class="mb-0">La météo du chômage</h2>
-      <p class="mt-0"><sub>Ou encore: &laquo; est-ce le meilleur moment de se faire salement lourder par son boss ? &raquo;</sub></p>
+      <p class="mt-0"><sub>Ou encore: &laquo;&nbsp;est-ce le meilleur moment de se faire salement lourder par son boss&nbsp;?&nbsp;&raquo;</sub></p>
 
       <hr />
     </header>
