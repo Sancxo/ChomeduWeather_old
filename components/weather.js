ae557c35be046a3d8bae7e5ea11e4bc056c94db0
Simon <simon@7lieues.io>
2022-11-23 16:13:48 +0100

    refactor(api): splits weather.js file into 2 files (weather.js for the component and shared/api-helpers.js for requests functions and managing the data)

--- components/weather.js
+++ components/weather.js
@@ -1,74 +1,31 @@
+import { nestedDataTable, lastUpdate, loadingApi } from "../shared/api-helper";
+
 export default async function weather() {
-  let loading = true;
-
-  async function fetchData() {
-    return await fetch(
-      'https://bdm.insee.fr/series/sdmx/data/SERIES_BDM/001688527',
-      {
-        method: 'GET',
-        cache: "no-cache",
-        headers: {
-          'Accept': "application/vnd.sdmx.structurespecificdata+xml;version=2.1",
-        }
-      }
-    )
-      .then(resp => {
-        loading = false;
-        return resp.text()
-      })
-      .catch(err => {
-        console.error(err);
-      })
-  }
-
-  function createDataObject(xmlData) {
-    let object = new Object();
-    const body = xmlData.getElementsByTagName("Obs");
-
-    // we want to return an object with this format { 2022: { Q1: "123", Q2: "456", Q3: "789", ...} }
-    for (let i = 0; i < body.length; i++) {
-      const [year, quarter] = body[i].attributes["TIME_PERIOD"].nodeValue.split("-"); // we split from "2022-Q3" to "2022" and "Q3"
-      if (object[year] === undefined) { // if year is not met yet, we write it inside object
-        object[year] = {}
-      }
-
-      object[year][quarter] = body[i].attributes["OBS_VALUE"].nodeValue;
-    }
-
-    return object;
-  }
-
-  const xml = await fetchData();
-  const rawData = new window.DOMParser().parseFromString(xml, "application/xml");
-
-  const lastUpdate = rawData.getElementsByTagName("Series")[0].attributes["LAST_UPDATE"].nodeValue;
-  const dataTable = createDataObject(rawData);
-
-  // console.log(dataTable)
+  // console.log(nestedDataTable)
 
   const lastYearsList = []
   // we gonna push the two last years into lastYearsList
   for (let i = 0; i < 2; i++) {
-    lastYearsList.push(Object.keys(dataTable).reverse()[i])
+    lastYearsList.push(Object.keys(nestedDataTable).reverse()[i])
   }
 
   const currentYear = lastYearsList[0];
 
-  const currentYearQuarterList = Object.keys(dataTable[currentYear]); // list of every quarter passed this year from newer to older
+  const currentYearQuarterList = Object.keys(nestedDataTable[currentYear]); // list of every quarter passed this year from newer to older
   const currentQuarter = currentYearQuarterList[0];
 
   // I current_quarter is "Q1", the previous one is "Q4" from previous year; if not it's previous quarter from same year.
   const previousOrCurrentYear = currentQuarter === "Q1" ? lastYearsList[1] : currentYear;
   const previousQuarter = currentQuarter === "Q1" ? "Q4" : currentYearQuarterList[1]
 
-  const lastIndicator = dataTable[currentYear][currentQuarter];
-  const previousIndicator = dataTable[previousOrCurrentYear][previousQuarter];
+  const lastIndicator = nestedDataTable[currentYear][currentQuarter];
+  const previousIndicator = nestedDataTable[previousOrCurrentYear][previousQuarter];
 
   const diff = (lastIndicator - previousIndicator).toFixed(1);
 
   const condition = lastIndicator < 9 && diff < 0.8 ? true : false;
 
-  if (loading) {
+  if (loadingApi) {
     return `Loading ...`
   } else {
     return `
---
+++ shared/api-helper.js
@@ -0,0 +1,61 @@
+let loadingApi = true;
+
+async function fetchData() {
+  return await fetch(
+    'https://bdm.insee.fr/series/sdmx/data/SERIES_BDM/001688527',
+    {
+      method: 'GET',
+      cache: "no-cache",
+      headers: {
+        'Accept': "application/vnd.sdmx.structurespecificdata+xml;version=2.1",
+      }
+    }
+  )
+    .then(resp => {
+      loadingApi = false;
+      return resp.text()
+    })
+    .catch(err => {
+      console.error(err);
+    })
+}
+
+function createDataObject(data) {
+  const timePeriods = [];
+  const values = [];
+
+  for (let i = 0; i < data.length; i++) {
+    timePeriods.push(data[i].attributes["TIME_PERIOD"].nodeValue);
+    values.push(data[i].attributes["OBS_VALUE"].nodeValue);
+  }
+
+  return [timePeriods, values]
+}
+
+function createNestedDataObject(timePeriods, values) {
+  const object = new Object();
+
+  // we want to return an object with this format { 2022: { Q1: "123", Q2: "456", Q3: "789", ...} }
+  for (let i = 0; i < timePeriods.length; i++) {
+    const [year, quarter] = timePeriods[i].split("-"); // we split from "2022-Q3" to "2022" and "Q3"
+    if (object[year] === undefined) { // if year is not met yet, we write it inside object
+      object[year] = {}
+    }
+
+    object[year][quarter] = values[i];
+  }
+
+  return object;
+}
+
+const xml = await fetchData();
+const parsedXml = new window.DOMParser().parseFromString(xml, "application/xml");
+
+const dataCollection = parsedXml.getElementsByTagName("Obs");
+
+const [timePeriods, values] = createDataObject(dataCollection);
+
+const nestedDataTable = createNestedDataObject(timePeriods, values);
+const lastUpdate = parsedXml.getElementsByTagName("Series")[0].attributes["LAST_UPDATE"].nodeValue;
+
+export { loadingApi, lastUpdate, timePeriods, values, nestedDataTable };
